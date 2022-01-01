4d3f8904c2ad84fa0e646972e2e03c6e89c61f26
Mitchell Szczepanczyk <mitchell@szcz.org>
2021-12-31 21:47:29 -0600

    Lots of changes.

--- JeopardyHeatmap.elm
+++ JeopardyHeatmap.elm
@@ -1,65 +1,74 @@
 module Main exposing (..)
 
+import Browser
 import Html exposing (div, text, p)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+import String exposing (fromInt)
 
 
 -- MODEL
 
+type AnswerStatus = Unread | Correct | Incorrect
 
 type alias JeopardyAnswer =
-    { file : Int
-    , rank : Int
+    { id : Int
+    , status : AnswerStatus
     }
 
-
-type alias Model =
-    { answers : List JeopardyAnswer
-    }
+type alias Model = List JeopardyAnswer
 
 
-ranksAndFiles : List ( Int, Int )
-ranksAndFiles =
-    List.concatMap (\n -> List.map ((,) n) <| List.range 1 5) <|
-        List.range 1 6
-
-
-initModel : Model
+-- initModel : Model
 initModel =
-    { answers = List.map (\( x, y ) -> JeopardyAnswer x y) ranksAndFiles }
-
-
+    List.map (\x -> JeopardyAnswer x Unread) (List.range 1 30)
 
 -- UPDATE
 
-
-type Msg
-    = Increment
-    | Decrement
-
-
 update msg model =
-    case msg of
-        Increment ->
-            model
-
-        Decrement ->
-            model
-
-
+  model
 
 -- VIEW
 
+getXCoord : Int -> Int
+getXCoord n = 
+  if modBy 6 n == 1 then 10
+  else if modBy 6 n == 2 then 160
+  else if modBy 6 n == 3 then 310
+  else if modBy 6 n == 4 then 460
+  else if modBy 6 n == 5 then 610
+  else if modBy 6 n == 0 then 760
+  else 1
+
+getYCoord : Int -> Int
+getYCoord n = 
+  if n < 7 then 
+    10
+  else if n < 13 && n > 6 then 
+    120
+  else if n < 19 && n > 12 then
+    230
+  else if n < 25 && n > 18 then
+    340
+  else
+    450
+
+getColor : AnswerStatus -> String
+getColor s =
+  case s of
+    Unread -> "blue"
+    Correct -> "green"
+    Incorrect -> "red"
+
 makeRectangle : JeopardyAnswer -> Svg msg
 makeRectangle answer =
-  rect [ x (toString (200 * answer.file))
-        , y (toString (150 * answer.rank))
-        , width "200"
-        , height "150"
-        , fill "blue"
+  rect [ x (fromInt (getXCoord answer.id))
+        , y (fromInt (getYCoord answer.id))
+        , width "140"
+        , height "100"
+        , fill (getColor answer.status)
         , stroke "black"
-        , strokeWidth "3"
+        , strokeWidth "2"
         ] []
 
 
@@ -70,14 +79,13 @@ gameBoard model =
     , width "2000"
     , height "1200"
     ] 
-    (List.map makeRectangle model.answers)
+    (List.map makeRectangle model)
 
 
 view model =
     div []
         [ (gameBoard model)
         , p [] []
-        , Html.text (toString model)
         ]
 
 
@@ -85,8 +93,8 @@ view model =
 
 
 main =
-    Html.beginnerProgram
-        { model = initModel
+    Browser.sandbox
+        { init = initModel
         , update = update
         , view = view
         }
--- src/Main.elm
+++ src/Main.elm
@@ -6,63 +6,89 @@ import Html.Events exposing (onClick)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
 
+
+
 -- MAIN
 
+
 main =
-  Browser.sandbox { init = init, update = update, view = view }
+    Browser.sandbox { init = init, update = update, view = view }
+
+
 
 -- MODEL
 
-type Response = Correct | Incorrect | Unanswered
 
-type alias Model = { content : Response}
+type Response
+    = Correct
+    | Incorrect
+    | Unanswered
+
+
+type alias Model =
+    { content : Response }
 
 
 init : Model
-init = { content = Unanswered}
+init =
+    { content = Unanswered }
+
+
 
 -- UPDATE
 
 
 type Msg
-  = SetCorrect
-  | SetIncorrect
+    = SetCorrect
+    | SetIncorrect
 
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    SetCorrect ->
-      { model | content = Correct }
+    case msg of
+        SetCorrect ->
+            { model | content = Correct }
 
-    SetIncorrect ->
-      { model | content = Incorrect }
+        SetIncorrect ->
+            { model | content = Incorrect }
+
+
 
 -- VIEW
 
+
 setColor : Response -> String
 setColor response =
-  case response of
-    Correct -> "#12E603"
-    Incorrect -> "red"
-    _ -> "blue"
+    case response of
+        Correct ->
+            "#12E603"
+
+        Incorrect ->
+            "red"
+
+        _ ->
+            "blue"
+
 
 view : Model -> Html Msg
 view model =
-  div []
-    [ svg [
-    viewBox "0 0 200 200"
-    , width "200"
-    , height "200"
-    ] [rect
-        [ x "0"
-        , y "0"
-        , width "100"
-        , height "100"
-        , fill (setColor model.content)
-        , stroke "black"
-        , strokeWidth "2"
+    div []
+        [ svg
+            [ viewBox "0 0 200 200"
+            , width "200"
+            , height "200"
+            ]
+            [ rect
+                [ x "0"
+                , y "0"
+                , width "100"
+                , height "100"
+                , fill (setColor model.content)
+                , stroke "black"
+                , strokeWidth "2"
+                ]
+                []
+            ]
+        , button [ onClick SetCorrect ] [ Html.text "Yes" ]
+        , button [ onClick SetIncorrect ] [ Html.text "No" ]
         ]
-        []]
-    , button [ onClick SetCorrect ] [ Html.text "Yes" ]
-    , button [ onClick SetIncorrect ] [ Html.text "No" ]]
